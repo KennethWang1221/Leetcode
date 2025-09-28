8dfcc475a046107294d4bd31e93a98e4fb4ca9f0
xwang150@lakeheadu.ca <xwang150@lakeheadu.ca>
2025-09-27 17:19:00 -0700

    Leetcode-0322: Recap Leetcode_0322_Coin_Change.cpp using DP

--- data_structures/Leetcode_0322_Coin_Change.cpp
+++ data_structures/Leetcode_0322_Coin_Change.cpp
@@ -9,26 +9,34 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX));
-        
-        // Base case: 0 coins needed to make amount 0
-        for (int i = 0; i <= n; ++i) {
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, INT_MAX));
+
+        for (int i=0;i<n+1;i++){
             dp[i][0] = 0;
         }
-        
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= amount; ++j) {
-                int remain = j - coins[i - 1];
-                if (remain < 0) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    int include = (dp[i][remain] != INT_MAX) ? (1 + dp[i][remain]) : INT_MAX;
-                    dp[i][j] = min(dp[i - 1][j], include);
+
+        for (int i=1;i<n+1;i++){
+            for (int j=1;j<amount+1;j++){
+                int remain = j - coins[i-1];
+                if(remain < 0){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    if (dp[i][remain] == INT_MAX){
+                        dp[i][j] = dp[i-1][j];
+                    }else{
+                        dp[i][j] = min(1 + dp[i][remain], dp[i-1][j]);
+                    }
+                    
                 }
             }
         }
         
-        return (dp[n][amount] == INT_MAX) ? -1 : dp[n][amount];
+        if (dp[n][amount] != INT_MAX){
+            return dp[n][amount];
+        }else{
+            return -1;
+        }
+        
     }
 };
 
